406cbef9e7c2e55aa6c5e9ec966bfa3c695a7ef7
shubhamgiri9548 <ankushgiri2007@gmail.com>
2026-08-13 13:37:15 +0530

    Fix production authentication cookies

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -60,7 +60,10 @@ export const login = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite:
+        process.env.NODE_ENV === "production"
+          ? "none"
+          : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
